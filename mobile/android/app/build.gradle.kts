726e6a95a583ff1687e83b964cce2e7cbbb042cc
Leopold Johannes Meinel <leo@meinel.dev>
2026-04-23 06:51:39 +0200

    chore(version): Bump to 0.26.6

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -55,9 +55,9 @@ android {
         minSdk = 26
         targetSdk = 37
         // NOTE: Increase by 1 on each release
-        versionCode = 57
+        versionCode = 58
         // NOTE: Update with full semantic version on each release
-        versionName = "0.26.5"
+        versionName = "0.26.6"
         // https://developer.android.com/reference/tools/gradle-api/9.1/com/android/build/api/variant/ExternalNativeBuild
         // NOTE: We need this, otherwise libc++_shared.so will not be inserted
         @Suppress("UnstableApiUsage")
